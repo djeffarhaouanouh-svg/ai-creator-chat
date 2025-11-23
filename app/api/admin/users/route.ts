e23d7c73024e6d9f35ceeafee126d2e2af5a3e9d
djeffarhaouanouh-svg <djeffar.haouanouh@gmail.com>
2025-11-23 23:39:57 +0100

    Update

--- app/api/admin/users/route.ts
+++ app/api/admin/users/route.ts
@@ -1,4 +1,5 @@
- import { NextResponse } from 'next/server'
+export const dynamic = "force-dynamic";
+import { NextResponse } from 'next/server'
 import { supabase } from '@/lib/supabase'
 
 export async function GET(request: Request) {
